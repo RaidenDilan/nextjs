52aaca24b58a2afa2bf3987dcd03a1db49255a22
RaidenDilan <raiden18@me.com>
2020-04-13 18:46:08 +0100

    A Special Lifecycle Hook

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-  <div>
-    <h1>The Main Page</h1>
-    <p>
-      Go to
-      <Link href='/auth'><a>Auth</a></Link>
-    </p>
-    <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-  </div>
-);
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    console.log(context);
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve({ appName: 'Super App' }), 1000);
+    });
 
-export default indexPage;
+    promise
+      .then((result) => console.log('Promise -> result', result))
+      .catch((error) => console.log('Promise -> error', error));
+
+    return promise;
+  }
+  // static async getInitialProps(context) {
+  //   console.log(context);
+  //   return { appName: 'Super App' };
+  // }
+
+  render() {
+    return(
+      <div>
+        <h1>The Main Page of { this.props.appName }</h1>
+        <p>
+          Go to
+          <Link href='/auth'><a>Auth</a></Link>
+        </p>
+        <button onClick={ () => Router.push('/auth') }>Go to Auth</button>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
